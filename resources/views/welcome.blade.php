a06eb68376de041d7ec33af6e237f5a17eb62833
BrentAlbers <93827410+BrentAlbers@users.noreply.github.com>
2022-09-27 11:18:07 +0200

    #Grote commit
    Laatste grote commit want kreeg het niet helemaal voor elkaar om het apart te doen.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,11 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
-<body>
     
         <header>
             <img src="img/logo.png" alt="">
         </header>
-        <div class="container">
+
         <body>
             <div class="deSchool">
                 <img class="jim" src="img/improvedjim.png" alt="">
@@ -41,17 +40,32 @@
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto impedit architecto dolore, omnis maxime consequuntur voluptas accusantium sapiente reprehenderit illo aliquid fuga totam quam eos vitae quos repellat quo repudiandae? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia doloribus, reiciendis magnam laborum et illum at debitis mollitia porro a quaerat quas voluptates quae ullam cupiditate dignissimos pariatur voluptate recusandae.</p>
                 </div>
             </div>
-            <div class="vakken">
-                <h2>vakken</h2>
-                <ul>
-                    <li><h3>Nederlands</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ea minima neque consectetur earum. Quis, vero, doloribus officia error eum itaque culpa provident quidem nisi dolores assumenda ex nostrum soluta?</p></li>
-                    <li><h3>Rekenen</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ea minima neque consectetur earum. Quis, vero, doloribus officia error eum itaque culpa provident quidem nisi dolores assumenda ex nostrum soluta?</p></li>
-                    <li><h3>Engels</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ea minima neque consectetur earum. Quis, vero, doloribus officia error eum itaque culpa provident quidem nisi dolores assumenda ex nostrum soluta?</p></li>
-                    <li><h3>Web</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ea minima neque consectetur earum. Quis, vero, doloribus officia error eum itaque culpa provident quidem nisi dolores assumenda ex nostrum soluta?</p></li>
-                    <li><h3>Win</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ea minima neque consectetur earum. Quis, vero, doloribus officia error eum itaque culpa provident quidem nisi dolores assumenda ex nostrum soluta?</p></li>
-                </ul>
+            <div class="container">
+                <img class="blokimg" src="img/gebouw.jpg" alt="">
+                <div class="blokken">
+                    <h2>Blokken</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem totam placeat, corrupti vel repellendus qui fugiat hic eligendi odio dolorem nemo illo eos fuga rerum laborum beatae ipsum blanditiis repellat?</p>
+                    <ul>
+                        <li><span>Blok A</span><br> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi expedita eaque architecto vero illum dolorum quidem et dicta ipsa consectetur quos hic aliquam sit numquam, voluptatum nam harum labore qui.</li>
+                        <li><span>Blok B</span><br>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ipsum esse tempore, dolores quod natus, eos eum aspernatur, nostrum sequi saepe nihil consequatur veritatis quo numquam quaerat nulla consequuntur dolorem.</li>
+                    </ul>
+                </div>
+
+
+                <!-- <div class="vakken">
+                    <h2>vakken</h2>
+                    <ul>
+                        <li><h3>Nederlands</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ea minima neque consectetur earum. Quis, vero, doloribus officia error eum itaque culpa provident quidem nisi dolores assumenda ex nostrum soluta?</p></li>
+                        <li><h3>Rekenen</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ea minima neque consectetur earum. Quis, vero, doloribus officia error eum itaque culpa provident quidem nisi dolores assumenda ex nostrum soluta?</p></li>
+                        <li><h3>Engels</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ea minima neque consectetur earum. Quis, vero, doloribus officia error eum itaque culpa provident quidem nisi dolores assumenda ex nostrum soluta?</p></li>
+                        <li><h3>Web</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ea minima neque consectetur earum. Quis, vero, doloribus officia error eum itaque culpa provident quidem nisi dolores assumenda ex nostrum soluta?</p></li>
+                        <li><h3>Win</h3><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ea minima neque consectetur earum. Quis, vero, doloribus officia error eum itaque culpa provident quidem nisi dolores assumenda ex nostrum soluta?</p></li>
+                    </ul>
+                </div> -->
             </div>
         </body>
-    </div>
-</body>
+        <footer>
+
+        </footer>
+
 </html>
